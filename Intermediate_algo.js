cdbbea46a3af7a78d3bf3745099185455d446a5d
Aiman Nazmi <aimannazmi59@gmail.com>
2021-09-01 23:36:50 +0800

    first_question

---
+++ Intermediate_algo.js
@@ -0,0 +1,14 @@
+//Sum All Numbers In A Range
+
+// function sumAll(arr) {
+//     var maxNumber = Math.max(arr[0], arr[1]);
+//     var minNumber = Math.min(arr[0], arr[1]);
+//     var sumNum = 0;
+
+//     for(var i = minNumber; i <= maxNumber; i++) {
+//         sumNum += i;
+//     }
+//     return sumNum;
+// }
+  
+// console.log(sumAll([1, 4]));
