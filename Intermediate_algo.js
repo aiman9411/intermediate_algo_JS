4116729ecf85b5d794648eb55a8742f043d0f654
Aiman Nazmi <aimannazmi59@gmail.com>
2021-09-01 23:43:55 +0800

    Q2 Diff Two Arrays

--- Intermediate_algo.js
+++ Intermediate_algo.js
@@ -12,3 +12,11 @@
 // }
   
 // console.log(sumAll([1, 4]));
+
+//Q2 Diff Two Arrays
+
+// function diffArray(arr1, arr2) {
+//     return arr1.concat(arr2).filter(element => !arr1.includes(element) || !arr2.includes(element))
+// };
+  
+// console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
