e8873a4cce1a1e7de960a9d390f289fb5201d145
Aiman Nazmi <aimannazmi59@gmail.com>
2021-09-02 23:50:04 +0800

    Q4 Wherefore art thou

--- Intermediate_algo.js
+++ Intermediate_algo.js
@@ -32,3 +32,17 @@
 //   }
   
 //   console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+
+//Q4 Wherefore art thou
+
+// function whatIsInAName(collection, source) {
+//     var srcKeys = Object.keys(source);
+  
+//     return collection.filter(function(obj) {
+//       return srcKeys.every(function(key) {
+//         return obj.hasOwnProperty(key) && obj[key] === source[key];
+//       });
+//     });
+//   }
+  
+//   console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
