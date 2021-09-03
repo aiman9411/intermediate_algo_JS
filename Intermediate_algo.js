b2ca03da2dd62f6675a15c4cef32147cc5d72798
Aiman Nazmi <aimannazmi59@gmail.com>
2021-09-03 23:09:32 +0800

    Update Q4

--- Intermediate_algo.js
+++ Intermediate_algo.js
@@ -37,12 +37,14 @@
 
 // function whatIsInAName(collection, source) {
 //     var srcKeys = Object.keys(source);
-  
 //     return collection.filter(function(obj) {
-//       return srcKeys.every(function(key) {
-//         return obj.hasOwnProperty(key) && obj[key] === source[key];
-//       });
-//     });
-//   }
+//         for(var i = 0; i < srcKeys.length; i++) {
+//             if(!obj.hasOwnProperty(srcKeys[i]) || obj[srcKeys[i]] !== source[srcKeys[i]]) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     })
+// }
   
 //   console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
