406dda1cdeb46f0d724ec7054c6b0481f97df96b
Aiman Nazmi <aimannazmi59@gmail.com>
2021-09-06 23:21:18 +0800

    Q7 Search & Replace

--- Intermediate_algo.js
+++ Intermediate_algo.js
@@ -75,3 +75,16 @@
 // }
 
 // console.log(translatePigLatin("glove"));
+
+//Q7 Search & Replace
+
+// function myReplace(str, before, after) {
+//     if(before[0] === before[0].toUpperCase()) {
+//         after = after[0].toUpperCase() + after.slice(1)
+//     } else {
+//         after = after[0].toLowerCase() + after.slice(1)
+//     }
+//     return str.replace(before, after);
+// }
+
+// console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"));
