6d07f833aeeb235b426f84d5b3bf30ba7c7cc1db
Aiman Nazmi <aimannazmi59@gmail.com>
2021-09-09 23:53:30 +0800

    Q10 Sorted Union

--- Intermediate_algo.js
+++ Intermediate_algo.js
@@ -126,3 +126,20 @@
 // }
   
 // console.log(fearNotLetter("abde"));
+
+// Q10 Sorted Union
+
+// function uniteUnique(arr) {
+//     var args = [...arguments];
+//     var result = [];
+//     for (var i = 0; i < args.length; i++) {
+//       for (var j = 0; j < args[i].length; j++) {
+//         if (!result.includes(args[i][j])) {
+//           result.push(args[i][j]);
+//         }
+//       }
+//     }
+//     return result;
+//   }
+
+// console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
